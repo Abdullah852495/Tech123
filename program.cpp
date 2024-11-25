2b2c8921d05faaadcb850c4552a8d3d8497f98ad
Abdullah852495 <abdullah852495@gmail.com>
2024-11-25 14:19:35 +0500

    program.cpp

--- program.cpp
+++ program.cpp
@@ -1,9 +1,17 @@
 #include<iostream>
 using namespace std;
 int main()
-// printing Hello World
+// Getting data from user
 {
-cout<<"Hello world";
+int n1,n2,n3;
+ cout<<"Enter your first number";
+cin>>n1;
+ cout<<"Enter second number";
+ cin>>n2;
+ cout<<"Enter third program";
+ cin>>n3;
+ cout<<"Maximum number is :"<<n1>n2>n3<<endl;
+ 
 }
 
 
